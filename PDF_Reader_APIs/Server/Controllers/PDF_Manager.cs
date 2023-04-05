571268c44fec122152a1187468b01e698beff3ab
Saucter <alhosni1615@gmail.com>
2023-04-05 11:50:17 +0400

    Add template to APIs

--- PDF_Reader_APIs/Server/Controllers/PDF_Manager.cs
+++ PDF_Reader_APIs/Server/Controllers/PDF_Manager.cs
@@ -13,14 +13,36 @@ public class PDF_Manager : ControllerBase
     [HttpPost]
     public async Task<ActionResult<int>> PostPDF()
     {
-
         return Ok();
     }
 
     [HttpGet]
-    public async Task<ActionResult<List<PDF>>> GetPDFs(int id)
+    public async Task<ActionResult<List<PDF>>> GetPDFs(int[]? id)
     {
-        
+        return Ok();
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<PDF>>> GetKeyword(int[]? id, string Keyword, bool? Exact)
+    {
+        return Ok();
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<PDF>>> GetTopWords(int[]? id, string? Ignore)
+    {
+        return Ok();
+    }
+
+    [HttpDelete]
+    public async Task<ActionResult<List<PDF>>> DeletePDF(int[] id)
+    {
+        return Ok();
+    }
+
+    [HttpDelete]
+    public async Task<ActionResult<List<PDF>>> DeleteAll()
+    {
         return Ok();
     }
 }

93ac365c7e6c556fbb1a17de6484e014abece414
Saucter <alhosni1615@gmail.com>
2023-04-14 17:00:07 +0400

    Add basic authentication handler middleware

---
+++ PDF_Reader_APIs/Server/Authentication/BasicAuthenticationHandler.cs
@@ -0,0 +1,39 @@
+using System.Security.Claims;
+using System.Text;
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Options;
+
+namespace PDF_Reader_APIs.Server.Authentication
+{
+    public class BasicAuthenticationHandler : AuthenticationHandler<User>
+    {
+        private readonly IUserRepository UserAuthenticator;
+        public BasicAuthenticationHandler(IOptionsMonitor<User> options, ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock, IUserRepository UserAuthenticator)
+        : base(options, logger, encoder, clock)
+        {
+            this.UserAuthenticator = UserAuthenticator;
+        }
+
+        protected async override Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            var authorizationHeader = Request.Headers["Authorization"].ToString();
+            if (authorizationHeader != null && authorizationHeader.StartsWith("basic", StringComparison.OrdinalIgnoreCase))
+            {
+                var token = authorizationHeader.Substring("Basic ".Length).Trim();
+                var credentialsAsEncodedString = Encoding.UTF8.GetString(Convert.FromBase64String(token));
+                var credentials = credentialsAsEncodedString.Split(':');
+                if (await UserAuthenticator.Authenticate(credentials[0], credentials[1]))
+                {
+                    var claims = new[] { new Claim("name", credentials[0]), new Claim(ClaimTypes.Role, "Admin") };
+                    var identity = new ClaimsIdentity(claims, "Basic");
+                    var claimsPrincipal = new ClaimsPrincipal(identity);
+                    return await Task.FromResult(AuthenticateResult.Success(new AuthenticationTicket(claimsPrincipal, Scheme.Name)));
+                }
+            }
+            Response.StatusCode = 401;
+            Response.Headers.Add("WWW-Authenticate", "Basic realm=\"PDF_Reader_API\"");
+            return await Task.FromResult(AuthenticateResult.Fail("Invalid Authorization Header"));
+        }
+    }  
+}
